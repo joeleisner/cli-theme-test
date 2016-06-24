80819842e6a38f8b5f3d310753a45e1b5c30833d
Joel Eisner <eisnerjr@vcu.edu>
2016-06-23 21:11:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node --harmony
+var chalk = require('chalk');
+
+var output = [
+    chalk.bold('bold'),
+    chalk.dim('dim'),
+    chalk.italic('italic'),
+    chalk.inverse('inverse'),
+    chalk.strikethrough('strikethrough'),
+    chalk.black('black'),
+    chalk.red('red'),
+    chalk.green('green'),
+    chalk.yellow('yellow'),
+    chalk.blue('blue'),
+    chalk.magenta('magenta'),
+    chalk.cyan('cyan'),
+    chalk.white('white'),
+    chalk.gray('gray'),
+    chalk.bgBlack('bgBlack'),
+    chalk.black.bgRed('bgRed'),
+    chalk.black.bgGreen('bgGreen'),
+    chalk.black.bgYellow('bgYellow'),
+    chalk.bgBlue('bgBlue'),
+    chalk.black.bgMagenta('bgMagenta'),
+    chalk.black.bgCyan('bgCyan'),
+    chalk.black.bgWhite('bgWhite')
+].join('\n');
+
+console.log(output);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# CLI Theme Test
+A simple tool that allows you to check your command-line theme.
+
+## How to Install
+```
+$ npm i -g cli-theme-test
+```
+
+## How to Use
+If you've installed the package globally, just open up your command-line tool of choice and run the following command:
+```
+$ cli-theme-test
+```
