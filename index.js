ebd86693d4d43f6ee9ef44197b6bb632de784630
Joel Eisner <jeisner93@gmail.com>
2018-08-12 11:26:21 -0400

    Updated the entry script (index.js) to drop harmony mode and to cleanup the syntax

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-#!/usr/bin/env node --harmony
-var chalk = require('chalk');
+#!/usr/bin/env node
 
-var output = [
-    chalk.bold('bold'),
-    chalk.dim('dim'),
-    chalk.italic('italic'),
-    chalk.inverse('inverse'),
-    chalk.strikethrough('strikethrough'),
-    chalk.black('black'),
-    chalk.red('red'),
-    chalk.green('green'),
-    chalk.yellow('yellow'),
-    chalk.blue('blue'),
-    chalk.magenta('magenta'),
-    chalk.cyan('cyan'),
-    chalk.white('white'),
-    chalk.gray('gray'),
-    chalk.bgBlack('bgBlack'),
-    chalk.black.bgRed('bgRed'),
-    chalk.black.bgGreen('bgGreen'),
-    chalk.black.bgYellow('bgYellow'),
-    chalk.bgBlue('bgBlue'),
-    chalk.black.bgMagenta('bgMagenta'),
-    chalk.black.bgCyan('bgCyan'),
-    chalk.black.bgWhite('bgWhite')
-].join('\n');
+const chalk = require('chalk'),
+    output  = [
+        chalk.bold('bold'),
+        chalk.dim('dim'),
+        chalk.italic('italic'),
+        chalk.inverse('inverse'),
+        chalk.strikethrough('strikethrough'),
+        chalk.black('black'),
+        chalk.red('red'),
+        chalk.green('green'),
+        chalk.yellow('yellow'),
+        chalk.blue('blue'),
+        chalk.magenta('magenta'),
+        chalk.cyan('cyan'),
+        chalk.white('white'),
+        chalk.gray('gray'),
+        chalk.bgBlack('bgBlack'),
+        chalk.black.bgRed('bgRed'),
+        chalk.black.bgGreen('bgGreen'),
+        chalk.black.bgYellow('bgYellow'),
+        chalk.bgBlue('bgBlue'),
+        chalk.black.bgMagenta('bgMagenta'),
+        chalk.black.bgCyan('bgCyan'),
+        chalk.black.bgWhite('bgWhite')
+    ];
 
-console.log(output);
+output.map(message => console.log(message));
