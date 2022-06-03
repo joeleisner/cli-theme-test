57e1485387009f10c18f40b46829e27473274089
Joel Eisner <jeisner93@gmail.com>
2022-06-02 22:30:24 -0400

    New text, background, and message options
    Using `commander`, you can now use options to alter the output
    of the CLI.
    
    - `-b, --background`: Limits the test to background colors
    - `-t, --text`: Limits the test to text colors
    
    Additionally, providing an optional `[message]` argument, you can change
    the text that appears in those colors.
    
    Using `commander` also provides a useful version/help interface to
    better understand the CLI.

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
+import { readFileSync } from 'node:fs';
+import { URL } from 'node:url';
 
-const output  = [
-    chalk.bold('bold'),
-    chalk.dim('dim'),
-    chalk.italic('italic'),
-    chalk.inverse('inverse'),
-    chalk.strikethrough('strikethrough'),
-    chalk.black('black'),
-    chalk.red('red'),
-    chalk.green('green'),
-    chalk.yellow('yellow'),
-    chalk.blue('blue'),
-    chalk.magenta('magenta'),
-    chalk.cyan('cyan'),
-    chalk.white('white'),
-    chalk.gray('gray'),
-    chalk.bgBlack('bgBlack'),
-    chalk.black.bgRed('bgRed'),
-    chalk.black.bgGreen('bgGreen'),
-    chalk.black.bgYellow('bgYellow'),
-    chalk.bgBlue('bgBlue'),
-    chalk.black.bgMagenta('bgMagenta'),
-    chalk.black.bgCyan('bgCyan'),
-    chalk.black.bgWhite('bgWhite')
-];
+const {
+    bin,
+    description,
+    version
+} = JSON.parse(readFileSync(new URL('./package.json', import.meta.url).pathname));
 
-output.forEach(message => console.log(message));
+const name = Object.keys(bin)[0];
+
+import { program } from 'commander';
+
+// Define the program options/arguments...
+program
+    .name(name)
+    .description(description)
+    .version(version)
+    .option('-b, --background', 'limit the test to background colors')
+    .option('-t, --text', 'limit the test to text colors')
+    .arguments('[message]', 'a message to print with the colors');
+// ... and parse it
+program.parse();
+
+// Get the program options
+const options = program.opts();
+
+// Define an empty color map...
+let colors = {};
+// ... and grab the text/background color maps
+import { text, background } from './colors.js';
+
+// If the background flag has not been used, add text colors to the color map
+if (!options.background || options.text) colors = { ...colors, ...text };
+
+// If the text flag has note been used, add background colors to the color map
+if (!options.text || options.background) colors = { ...colors, ...background };
+
+// For each entry in the color map,...
+Object.entries(colors).forEach(([name, color]) => {
+    // ... console log a defined message, or its name, using the color
+    console.log(color(program.args[0] || name));
+});
